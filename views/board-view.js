3f8d2647e3a041462308885e6f65c8759208da8c
mic_drop <miguel.saraiva@academiadecodigo.org>
2024-04-22 12:26:50 +0000

    adds all square rows to 1st round board

--- views/board-view.js
+++ views/board-view.js
@@ -4,20 +4,35 @@ import boardService from "../services/board-service.js";
 let boardView = {};
 
 boardView.show = function () {
+    let board = '<div id="board"></div>';
+    $(board).appendTo('#main-content');
+
     renderBoard();
     loadHandlers();
 }
 
-let renderBoard = function () {
-    let board = '<div id="board"></div>';
-    $(board).appendTo('#main-content');
-    renderInputRow()
-    renderSquareRow();
+let renderBoard = function (round) {
+
+    round = round || 0;
+    const cols = boardService.getCols();
+    const rows = boardService.getRows();
+
+    for (let row = 0; row < rows; row++) {
+        if (round === row) {
+            renderInputRow();
+            continue;
+        }
+        renderSquareRow(row);
+
+    }
 
 }
 
-let renderInputRow = function () {
 
+
+
+
+let renderInputRow = function () {
 
     const cols = boardService.getCols();
     let row = `<div id="row"></div>`;
@@ -32,13 +47,13 @@ let renderInputRow = function () {
     }
 }
 
-let renderSquareRow = function () {
+let renderSquareRow = function (currentRow) {
     const cols = boardService.getCols();
-    let row = `<div id="row-square"></div>`;
+    let row = `<div id="row-${currentRow}-square"></div>`;
     $(row).appendTo('#board');
     for (let i = 0; i < cols; i++) {
-        let square = `<div id="r2-i${i}" class="square"></div>`
-        $(square).appendTo('#row-square');
+        let square = `<div id="r${currentRow}-i${i}" class="square"></div>`
+        $(square).appendTo(`#row-${currentRow}-square`);
     }
 
 }
