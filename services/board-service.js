90e4790b3d47ef252572ca92aeaa4f234f55d910
mic_drop <miguel.saraiva@academiadecodigo.org>
2024-05-12 00:00:49 +0000

    creates CSS classes for guessed squares & updates to-do list

--- services/board-service.js
+++ services/board-service.js
@@ -2,7 +2,8 @@ let boardService = {
     getCols: () => 5,
     getRows: () => 6,
     currentRound: 0,
-    lastWord: []
+    lastWord: [],
+    correctWord: "tears"
 };
 
 boardService.maxRounds = boardService.getCols();
@@ -10,6 +11,26 @@ boardService.maxRounds = boardService.getCols();
 boardService.playRound = function (lastWord) {
     boardService.currentRound++;
     boardService.lastWord.push(lastWord);
+    checkWord(lastWord);
+}
+
+const checkWord = function (lastWord) {
+    const classes = [];
+    lastWord.forEach(letter => {
+        classes.push(checkLetter(letter));
+    });
+    return classes;
+}
+
+const checkLetter = function (letter, index) {
+    if (boardService.correctWord[index] === letter) {
+        return "correct";
+    }
+    if (boardService.correctWord.includes(letter)) {
+
+        return "miss";
+    }
+    return "wrong";
 }
 
 export default boardService;
