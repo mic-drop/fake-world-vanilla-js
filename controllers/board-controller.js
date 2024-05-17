5238285910d912f416177e90b01c09858db93d9d
mic_drop <miguel.saraiva@academiadecodigo.org>
2024-05-17 11:08:27 +0000

    adds back and forth focus

--- controllers/board-controller.js
+++ controllers/board-controller.js
@@ -1,10 +1,16 @@
 
+import boardService from "../services/board-service.js";
 import boardView from "./../views/board-view.js"
 
 let boardController = {};
 
 boardController.init = function () {
     console.log("board controller triggered");
+    boardView.show();
+}
+
+boardController.playRound = function (word) {
+    boardService.playRound(word);
     boardView.show();
 }
 
--- ui/board-ui.js
+++ ui/board-ui.js
@@ -13,18 +13,37 @@ export function loadHandlers(currentRow) {
                         return acc + $(element).val() || '';
                     }, '');
                     if (word.length === 5) {
-                        boardService.playRound(word);
-                        boardController.init();
+                        boardController.playRound(word);
+                        return;
                     }
+                }
+                if (e.key === 'Backspace') {
+                    goBackwards(element, e, currentRow, i);
                 }
             })
             return;
         }
         $(element).on("keyup", (e) => {
-            if ((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 97 && e.keyCode <= 122)) {
-                $(`#r${currentRow}-i${i + 1}`).focus();
+            defaultButton(e, currentRow, i);
+            if (i > 0) {
+                goBackwards(element, e, currentRow, i);
             }
         })
 
     });
+}
+
+const defaultButton = function (e, currentRow, index) {
+
+    if ((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 97 && e.keyCode <= 122)) {
+        $(`#r${currentRow}-i${index + 1}`).focus();
+        return;
+    }
+}
+
+const goBackwards = function (element, e, currentRow, index) {
+    if ($(element).val() === '' && e.key === 'Backspace') {
+        document.activeElement.blur();
+        $(`#r${currentRow}-i${index - 1}`).focus();
+    }
 }
