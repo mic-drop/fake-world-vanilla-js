1ed3e59a3df0b35642e6a4eb6675a174dc0b2417
mic_drop <miguel.saraiva@academiadecodigo.org>
2024-11-21 13:05:50 +0000

    implements-async-await by only rendering board after correctWord is not undefined

--- services/word-api-service.js
+++ services/word-api-service.js
@@ -1,10 +1,11 @@
 import board from "../model/board.js";
 
 const getWord = async function () {
-    const URL_API = "https://random-word-api.herokuapp.com/word?length=5";
+    const URL_API = "https://random-word-api.herokuapp.com/all";
     const response = await fetch(URL_API);
-    const word = await response.json();
-    board.correctWord = word[0];
+    const list = await response.json().then(list => list.filter(word => word.length === 5));
+    board.correctWord = await list[Math.floor(Math.random() * (list.length - 0 + 1)) + 0];
+    console.log("This is word " + board.correctWord);
 }
 
 export default getWord;
