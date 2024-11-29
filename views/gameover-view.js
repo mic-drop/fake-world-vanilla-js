e7eb0d2b8d340bbcee9afcae8ed104e450cebc60
mic_drop <miguel.saraiva@academiadecodigo.org>
2024-11-29 15:23:30 +0000

    changes the border radios for css class square

--- views/gameover-view.js
+++ views/gameover-view.js
@@ -1,13 +1,13 @@
 let gameOverView = {};
 
 gameOverView.show = function () {
-    let gameOverDiv = `<div id="gameover" class="won"> YOU WON!</div>`;
+    let gameOverDiv = `<div id="gameover" class="won square"> YOU WON!</div>`;
     $('#main-content').css('filter', 'blur(9px)');
     $('body').append(gameOverDiv);
 }
 
 gameOverView.lostShow = function (word) {
-    let lostView = `<div id="gameover" class="lost">
+    let lostView = `<div id="gameover" class="lost square">
      <p>YOU LOSE =( !</p>
      <p>Correct Word was </p>
      <h1 id="correct-word">${word.toUpperCase()}</h1>
